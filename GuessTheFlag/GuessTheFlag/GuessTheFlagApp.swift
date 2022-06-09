f4d5c48d22991a94cde2ad0781ae7a03cec22568
Sam <105581592+Exaeous@users.noreply.github.com>
2022-06-09 19:58:41 +0530

    Skeleton

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Samuel Pulukuri on 09/06/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
